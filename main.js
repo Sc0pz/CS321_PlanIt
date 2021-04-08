801228a1a57b208effa11c95461692db4dc042d1
Raymond Phan <raymondphan00@gmail.com>
2021-04-08 15:58:28 -0400

    Cleaned code

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron')
-const { menu, Menu } = require('./menu') 
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
-const isWindows = process.platform === "win32";
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame : false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -22,16 +21,6 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-
-  ipcMain.on(`display-app-menu`, function(e, args) {
-    if (isWindows && mainWindow) {
-      menu.popup({
-        window: mainWindow,
-        x: args.x,
-        y: args.y
-      });
-    }
-  });
 }
 
 // This method will be called when Electron has finished
